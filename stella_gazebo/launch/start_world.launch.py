8566da23fe927cf7d8fb94ab5a21970e37c4515a
ntrexlab <85467544+ntrexlab@users.noreply.github.com>
2024-06-25 15:59:01 +0900

    Add files via upload

---
+++ stella_gazebo/launch/start_world.launch.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_prefix
+
+def generate_launch_description():
+
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    pkg_stella_gazebo = get_package_share_directory('stella_gazebo')
+
+    description_package_name = 'stella_description'
+    install_dir = get_package_prefix(description_package_name)
+
+    gazebo_models_path = os.path.join(pkg_stella_gazebo, 'models')
+
+    if 'GAZEBO_MODEL_PATH' in os.environ:
+        os.environ['GAZEBO_MODEL_PATH'] = os.environ['GAZEBO_MODEL_PATH'] + ':' + install_dir + '/share' + ':' + gazebo_models_path
+    else:
+        os.environ['GAZEBO_MODEL_PATH'] = install_dir + "/share" + ':' + gazebo_models_path
+
+    if 'GAZEBO_PLUGIN_PATH' in os.environ:
+        os.environ['GAZEBO_PLUGIN_PATH'] = os.environ['GAZEBO_PLUGIN_PATH'] + ':' + install_dir + '/lib'
+    else:
+        os.environ['GAZEBO_PLUGIN_PATH'] = install_dir + '/lib'
+
+    print("GAZEBO MODELS PATH=="+str(os.environ['GAZEBO_MODEL_PATH']))
+    print("GAZEBO PLUGINS PATH=="+str(os.environ['GAZEBO_PLUGIN_PATH']))
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gazebo.launch.py'),
+        )
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'world',
+            default_value=[os.path.join(pkg_stella_gazebo, 'worlds', 'stella_empty.world'), ''],
+            description='SDF world file'),
+        gazebo
+    ])
